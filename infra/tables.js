9c7efdf8a6d7ab9dddcd736be9a09b3fe60442c7
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2021-09-07 18:57:20 -0300

    feat: CORS, env, procfile

--- infra/tables.js
+++ infra/tables.js
@@ -7,38 +7,38 @@ class Tables {
 
   createVoluntary() {
     const query = `
-      CREATE TABLE IF NOT EXISTS voluntaries (
-        id int NOT NULL AUTO_INCREMENT,
-        parentName varchar(255) NOT NULL,
-        age int NOT NULL,
-        schooling enum (
-          'Fundamental Incompleto',
-          'Fundamental Completo',
-          'Médio Incompleto',
-          'Médio Completo',
-          'Superior Incompleto',
-          'Superior Completo ou maior'
-        ) NOT NULL,
-        schoolingOther varchar(50),
-        income double(12,2),
-        incomeRefuse boolean,
-        relatives varchar(50) NOT NULL,
-        skinColor enum (
-          'Branca',
-          'Parda',
-          'Preta',
-          'Outra'
-        ) NOT NULL,
-        skinColorOther varchar(50),
-        city varchar(255) NOT NULL,
-        state varchar(100) NOT NULL,
-        childName varchar(255) NOT NULL,
-        childAge int NOT NULL,
-        procedureAge int NOT NULL,
-        followingCity varchar(255) NOT NULL,
-        followingState varchar(100) NOT NULL,
-        followingType enum ('Público', 'Privado', 'Misto') NOT NULL,
-        PRIMARY KEY(id))
+    CREATE TABLE IF NOT EXISTS voluntaries (
+      id int NOT NULL AUTO_INCREMENT,
+      parentName varchar(255) NOT NULL,
+      age int NOT NULL,
+      schooling enum (
+        'Fundamental Incompleto',
+        'Fundamental Completo',
+        'Médio Incompleto',
+        'Médio Completo',
+        'Superior Incompleto',
+        'Superior Completo ou maior'
+      ) NOT NULL,
+      schoolingOther varchar(50),
+      income double(12,2),
+      incomeRefuse boolean,
+      relatives varchar(50) NOT NULL,
+      skinColor enum (
+        'Branca',
+        'Parda',
+        'Preta',
+        'Outra'
+      ) NOT NULL,
+      skinColorOther varchar(50),
+      city varchar(255) NOT NULL,
+      state varchar(100) NOT NULL,
+      childName varchar(255) NOT NULL,
+      childAge int NOT NULL,
+      procedureAge int NOT NULL,
+      followingCity varchar(255) NOT NULL,
+      followingState varchar(100) NOT NULL,
+      followingType enum ('Público', 'Privado', 'Misto') NOT NULL,
+      PRIMARY KEY(id));
     `;
 
     this.connection.query(query, (error) => {
