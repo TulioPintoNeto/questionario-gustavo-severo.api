4ee602abd23bd57bb66f7cabdef1de3462e21e4a
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2021-09-08 01:08:24 -0300

    feat: Adding feedback to allowed URL

--- config/customExpress.js
+++ config/customExpress.js
@@ -6,6 +6,7 @@ module.exports = () => {
   const app = express();
 
   app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+  console.log(`Allowed CORS: ${process.env.ALLOWED_ORIGIN}`);
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
