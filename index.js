fcd1c64596d2a01689f1f29d7ea0110ed10377d4
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2021-12-20 23:13:03 -0300

    fix: Fixing cors origin to allow validapthsi-hcpa.com.br

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const Tables = require('./infra/tables');
 
 const app = express();
 
-app.use(cors({ origin: 'https://validapthsi-hcpa.com.br' }));
 app.use(express.json());
+app.use(() => {
+  app.use(cors({ origin: 'https://validapthsi-hcpa.com.br' }));
+});
 app.use(express.urlencoded({ extended: true }));
 consign().include('controllers').into(app);
 
